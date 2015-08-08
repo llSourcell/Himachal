cd9fde06a8462cc96d8b4ebf95b2ca9a1b9d3360
Siraj Raval <sirajraval1@gmail.com>
2015-08-08 15:43:50 -0700

    time to add discovery

---
+++ Himachal/HMDiscoveryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMDiscoveryViewController.h
+//  Himachal
+//
+//  Created by Siraj Ravel on 8/8/15.
+//  Copyright (c) 2015 Ellipse. All rights reserved.
+//
+
+#import "PFQueryTableViewController.h"
+
+@interface HMDiscoveryViewController : PFQueryTableViewController
+
+@end
---
+++ Himachal/HMDiscoveryViewController.m
@@ -0,0 +1,37 @@
+//
+//  HMDiscoveryViewController.m
+//  Himachal
+//
+//  Created by Siraj Ravel on 8/8/15.
+//  Copyright (c) 2015 Ellipse. All rights reserved.
+//
+
+#import "HMDiscoveryViewController.h"
+
+@interface HMDiscoveryViewController ()
+
+@end
+
+@implementation HMDiscoveryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
